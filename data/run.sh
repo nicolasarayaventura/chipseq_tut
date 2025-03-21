5066f632376cc82e377ae6f44b1abca5ccc759b6
Nicolas Araya Ventura <arayan01@li04e04.chimera.hpc.mssm.edu>
2025-03-21 16:05:59 -0400

    tutorial switch

--- data/run.sh
+++ data/run.sh
@@ -1,11 +1,11 @@
 set -x -e
 scratch="/sc/arion/scratch/arayan01/projects/chipseq_tut"
-function raw_data1 {
+function raw_data {
     sampledir="${scratch}/data/samples"
-        cat url_links.txt | while read sample accession url
+        cat url_links.txt | while read sample url
         do
-        wget -c -O "${sampledir}/${sample}_${accession}" "${url}"
+        wget -c -O "${sampledir}/${sample}.fastq.gz" "${url}"
         done < url_links.txt
 }
 
-raw_data1
+raw_data
